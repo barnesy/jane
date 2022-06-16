97da6301ca50f561839a3febbc5c43b7c93766e6
precious <chris@barnesy.me>
2022-06-16 11:14:38 -0400

    adds gay stuff

--- src/pages/GayStuff.vue
+++ src/pages/GayStuff.vue
@@ -1,8 +1,8 @@
 <template>
   <Directory
-    class="narrow dark"
-    bgColor="var(--light-blue)"
-    bgImage="url('/brown-to-green.jpg')"
+    class="narrow"
+    bgColor="#bbeca7"
+    bgImage="url('/red-to-orange.jpg')"
   >
     <template #page-header>
       <Header class="v2">
@@ -14,7 +14,6 @@
     </template>
 
     <PageSection
-      class="narrow left"
       bgColor="var(--light-grey)"
       padding="6rem 3rem 7rem"
     >
@@ -25,14 +24,205 @@
       <template>
        <CardGroup>
 
-         <Card url="http://tiktok.com">
+         <Card class="large" url="https://www.amazon.com/UBGICIG-Accessories-Bracelet-Adjustable-Rainbow/dp/B09S33DMWG/ref=sr_1_1?crid=3VMQDKKB2U9WD&keywords=gay+stuff&qid=1655385729&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C61&sr=1-1">
            <template #image>
-             <img src="https://www.amazon.com/UBGICIG-Accessories-Bracelet-Adjustable-Rainbow/dp/B09S33DMWG/ref=sr_1_1?crid=2ZE8OGN9NVNPM&keywords=gay+stuff&qid=1655307268&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C64&sr=1-1" />
+             <img src="https://m.media-amazon.com/images/I/916MuOTVOuL._AC_UL640_FMwebp_QL65_.jpg" />
            </template>
            <template>
-              braclets
+              braclets $18.56
            </template>
          </Card>
+
+          <Card url="https://www.amazon.com/Milkshake-Brings-Boys-Yard-T-Shirt/dp/B08FR1TY83/ref=sr_1_3?crid=2AS1CRTVTDS78&keywords=gay+stuff&qid=1655386128&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C49&sr=1-3">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/71G0agRwChL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              shirt $19.99
+           </template>
+         </Card>
+
+         <Card url="https://www.amazon.com/MEPLUM-Stickers-Bisexual-Colorful-Motorcycle/dp/B092PF2J5S/ref=sr_1_2?crid=2AS1CRTVTDS78&keywords=gay+stuff&qid=1655386230&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C49&sr=1-2">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/91AMWBPrjhL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+            stickers $14.99
+           </template>
+         </Card>
+
+         <Card url="https://www.amazon.com/Leg-Avenue-Womens-Rainbow-Thigh/dp/B002N2W3BM/ref=sr_1_8?crid=1BJ6MLI9PZEIC&keywords=gay+stuff&qid=1655386826&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C117&sr=1-8">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/61Qev3x5XZL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              socks $14.40
+           </template>
+         </Card>
+
+         <Card class="giant" url="https://www.amazon.com/Temporary-Stickers-Waterproof-Celebration-Supplies/dp/B0B12FPLGB/ref=sr_1_11?crid=21HQ8DVBPN4KA&keywords=gay+stuff&qid=1655386884&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C60&sr=1-11">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/61HAIKqSSKL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              tatoos $7.99
+           </template>
+         </Card>
+
+         <Card class="large" url="https://www.amazon.com/Guncle-Definition-Rainbow-Pride-T-Shirt/dp/B07L7W698N/ref=sr_1_7?crid=2LPH2MQ7HY5QA&keywords=gay+stuff&qid=1655387623&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C49&sr=1-7">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/71gtDjVPcbL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              shirt $14.99
+           </template>
+         </Card>
+
+          <Card url="https://www.amazon.com/Matching-Bracelets-Memorial-Decorations-Bisexual/dp/B09Z2L2DJ9/ref=sr_1_17?crid=2LPH2MQ7HY5QA&keywords=gay+stuff&qid=1655387623&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C49&sr=1-17">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/71zGHEhTr3L._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              pride party kit $13.99
+           </template>
+         </Card>
+
+         <Card url="https://www.amazon.com/Shhh-Nobody-Knows-Im-Lesbian/dp/B07N62G99X/ref=sr_1_20?crid=2LPH2MQ7HY5QA&keywords=gay+stuff&qid=1655387623&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C49&sr=1-20">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/71ImWhUKWhL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+             shirt $19.95
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/HUMAN-LGBT-Pride-Month-Transgender/dp/B07QGFC8QZ/ref=sr_1_21?crid=2LPH2MQ7HY5QA&keywords=gay+stuff&qid=1655387623&s=gift-cards&sprefix=gay+stuff%2Cgift-cards%2C49&sr=1-21">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/61e-5dNQDuL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              shirt $14.99
+           </template>
+         </Card>
+
+         <Card url="https://www.amazon.com/Proud-Pride-Transgender-Lesbian-T-Shirt/dp/B09Y14GR6N/ref=sr_1_22?crid=2FV9A9K13R45N&keywords=lesbian+shirt&qid=1655389437&sprefix=lesbian+shirt%2Caps%2C54&sr=8-22">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/616oD2pli1L._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              shirt $16.99
+           </template>
+         </Card>
+
+         <Card url="https://www.amazon.com/T-Shirt-Funny-Gay-Lesbian-Pride/dp/B07MLYXGMK/ref=sr_1_35?crid=2FV9A9K13R45N&keywords=lesbian+shirt&qid=1655389610&sprefix=lesbian+shirt%2Caps%2C54&sr=8-35">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/51f2bWRae8S._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              shirt $19.95
+           </template>
+         </Card>
+
+         <Card url="https://www.amazon.com/Funny-LGBTQ-Potato-Premium-T-Shirt/dp/B0B3DVYC3H/ref=sr_1_58_sspa?crid=2FV9A9K13R45N&keywords=lesbian+shirt&qid=1655389610&sprefix=lesbian+shirt%2Caps%2C54&sr=8-58-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExWkZBRVdLUjIyS0pNJmVuY3J5cHRlZElkPUEwODA4NjY5MjZRNjA5Q0NTRkxLMCZlbmNyeXB0ZWRBZElkPUEwNzYxOTUxS0VKMjVPUU1ESUg0JndpZGdldE5hbWU9c3BfYnRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/613wR4fq9bL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+             but it right now i love it $20.95
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/Lesbian-Flower-Gay-Parade-T-shirt/dp/B07FCTSQN9/ref=sr_1_47?crid=2FV9A9K13R45N&keywords=lesbian+shirt&qid=1655389610&sprefix=lesbian+shirt%2Caps%2C54&sr=8-47">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/71ZYoak12hL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+            shirt $13.07
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/Shout-Lesbian-Pride-LGBT-shirt/dp/B07DJGCN41/ref=sr_1_38?crid=2FV9A9K13R45N&keywords=lesbian+shirt&qid=1655389610&sprefix=lesbian+shirt%2Caps%2C54&sr=8-38">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/71360s9iP6L._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+            shirt $18.99
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/Proud-Smartass-Lesbian-Daughter-T-Shirt/dp/B09XMM1DN3/ref=sr_1_37?crid=2FV9A9K13R45N&keywords=lesbian+shirt&qid=1655389610&sprefix=lesbian+shirt%2Caps%2C54&sr=8-37">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/A1ntnF3PJOL._CLa%7C2140%2C2000%7C81hPMv1cjUL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX679_.png" />
+           </template>
+           <template>
+            shirt $16.99
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/Weekly-Agenda-Funny-Pride-Rainbow/dp/B07VT9Q8C9/ref=sr_1_34?crid=2FV9A9K13R45N&keywords=lesbian+shirt&qid=1655389610&sprefix=lesbian+shirt%2Caps%2C54&sr=8-34">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/714q1dZqJtL._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              shirt $19.99
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/I-support-gay-rats-T-Shirt/dp/B09MQSHG2W/ref=sr_1_3?crid=2K5A0OC200FAM&keywords=gay+rat+shirt&qid=1655390872&sprefix=gay+rat+shirt%2Caps%2C65&sr=8-3">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C811C-X9xRXL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX679_.png" />
+           </template>
+           <template>
+              shirt $19.99
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/Progress-flag-American-Decorations-Festival/dp/B099MVNT9H/ref=sr_1_1_sspa?crid=1KBABZHEN9RMH&keywords=gay%2Bstuff%2Bfor%2Bteens&qid=1655390978&sprefix=gay%2Bsttuf%2Caps%2C60&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExSlpYT0UyRVIxRVUwJmVuY3J5cHRlZElkPUEwMTM1ODAzMTIxTU80NzBVNVA4OCZlbmNyeXB0ZWRBZElkPUExMDQ1NjUwMUZKMjBMMVVOTzVEWCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU&th=1">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/91IkRkG5E2L._AC_UL640_FMwebp_QL65_.jpg" />
+           </template>
+           <template>
+              flags $14.99
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/Infinity-Republic-Bisexual-Pride-Blanket/dp/B08KWRVSL4/ref=sr_1_1_sspa?crid=2U47Q3R9E38AR&keywords=pride+stuffed+animal+bisexual&qid=1655391579&sprefix=pride+stuffed+animal+%2Caps%2C51&sr=8-1-spons&psc=1&smid=AQ94L35TDLVM6&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExT05LSzFIWTRaVTU3JmVuY3J5cHRlZElkPUEwNjM3MDY3VkxUNFBKVTc2M1lGJmVuY3J5cHRlZEFkSWQ9QTA1ODM2ODAyRklYM0lEQ0NTOUhIJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/71ii2vh53DL._AC_SX679_.jpg" />
+           </template>
+           <template>
+              bi blanket $19.95
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/Squishmallows-Kellytoy-Disney-Pride-Collection/dp/B0B27WHY9W/ref=sr_1_29?crid=2U47Q3R9E38AR&keywords=pride+stuffed+animal+bisexual&qid=1655391579&sprefix=pride+stuffed+animal+%2Caps%2C51&sr=8-29">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/71aGjQB-ZdL._AC_SX522_.jpg" />
+           </template>
+           <template>
+              stuffed animal $27.95
+           </template>
+         </Card>
+
+
+         <Card url="https://www.amazon.com/Pansexual-Rainbow-Stripe-Homosexuality-T-Shirt/dp/B08MB7R5G7/ref=sr_1_30_sspa?crid=2U47Q3R9E38AR&keywords=pride+stuffed+animal+bisexual&qid=1655391579&sprefix=pride+stuffed+animal+%2Caps%2C51&sr=8-30-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExT05LSzFIWTRaVTU3JmVuY3J5cHRlZElkPUEwNjM3MDY3VkxUNFBKVTc2M1lGJmVuY3J5cHRlZEFkSWQ9QTAyODc2MjMyMVhJVEFOS1pUQjQ3JndpZGdldE5hbWU9c3BfbXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==">
+           <template #image>
+             <img src="https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C81OXTtzyExL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX679_.png" />
+           </template>
+           <template>
+              shirt $19.99
+           </template>
+         </Card>
+
 
        </CardGroup>
       </template>
